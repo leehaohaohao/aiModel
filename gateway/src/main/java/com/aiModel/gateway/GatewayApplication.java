da16cfa56d757aababdbdf72cdec47bf4d99690a
lihao <805459342@qq.com>
2024-09-26 14:34:33 +0800

    -i init

---
+++ gateway/src/main/java/com/aiModel/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.aiModel.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lihao
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
